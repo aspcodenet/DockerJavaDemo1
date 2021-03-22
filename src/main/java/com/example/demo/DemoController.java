aa68c2851b743418e03a1a214372680ed6be9395
Stefan Holmberg <stefan.holmberg@systementor.se>
2021-03-22 16:05:43 +0100

    first commit

---
+++ src/main/java/com/example/demo/DemoController.java
@@ -0,0 +1,24 @@
+// ./mvnw.cmd clean package  -Pproduction -DskipTests
+
+package com.example.demo;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DemoController {
+    
+    @GetMapping(path="/player")    
+    @CrossOrigin()
+    List<String> getAll(){
+        var l = new ArrayList<String>();
+        l.add("Test");
+        l.add("Test2");
+        return l;
+    }
+    
+}
